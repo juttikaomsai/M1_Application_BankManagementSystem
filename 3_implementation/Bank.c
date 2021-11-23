a1d595b7889c4efde69097fd67a408100b6de668
Juttika Omsai <83201750+juttikaomsai@users.noreply.github.com>
2021-11-23 18:56:54 +0530

    Update Bank.c

--- 3_implementation/Bank.c
+++ 3_implementation/Bank.c
@@ -1,1 +1,61 @@
+#include "system.h"
+#include "Features.h"
+int i,j;
+void menu();
+int main_exit;
+int main()
+{
+    char pass[10],password[10]="pass";
+    int i=0;
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login ");
+    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+    scanf("%s",pass);
+    /*do
+    {
+    //if (pass[i]!=13&&pass[i]!=8)
+        {
+            printf("*");
+            pass[i]=getch();
+            i++;
+        }
+    }while (pass[i]!=13);
+    pass[10]='\0';*/
+    if (strcmp(pass,password)==0)
+        {printf("\n\nPassword Match!\nLOADING");
+        for(i=0;i<=6;i++)
+        {
+            fordelay(100000000);
+            printf(". ");
+        }
+        printf("\n");
+                system("pause");
+				system("cls");
+                
+            menu();
+        }
+    else
+        {   printf("\n\nWrong password!!\a\a\a");
+            login_try:
+            printf("\nEnter 1 to try again and 0 to exit:");
+            scanf("%d",&main_exit);
+            if (main_exit==1)
+                    {
+
+                        system("cls");
+                        main();
+                    }
+
+            else if (main_exit==0)
+                    {
+                    system("cls");
+                    close();}
+            else
+                    {printf("\nInvalid!");
+                    fordelay(1000000000);
+                    system("cls");
+                    goto login_try;}
+
+        }
+        return 0;
+}
 
